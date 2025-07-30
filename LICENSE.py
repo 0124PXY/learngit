90dea324a23667b72e4c83edd6538f82b4482dd8
pinocchio <e24301340@stu.ahu.edu.cn>
2025-07-30 16:26:28 +0800

    understand how stage works

---
+++ LICENSE.py
@@ -0,0 +1,8 @@
+class Student(object):
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def get_details(self):
+        return f"Name: {self.name}, Age: {self.age}"
+    
